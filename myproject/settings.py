7abe98a81d4f2ea7b296f31c254603577b0bc858
test <test>
2023-02-02 15:21:49 -1000

    Add requirements.txt file

--- myproject/settings.py
+++ myproject/settings.py
@@ -20,7 +20,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ftq-+m!d6-ya!*tr%z^i*6rpw3f84257@(^q5e1(_u3l-j)9p4'
+
+from decouple import config, Csv
+import dj_database_url
+SECRET_KEY = config('SECRET_KEY')
+DEBUG = config('DEBUG', default=False, cast=bool)
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
+}
+#SECRET_KEY = 'django-insecure-ftq-+m!d6-ya!*tr%z^i*6rpw3f84257@(^q5e1(_u3l-j)9p4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
